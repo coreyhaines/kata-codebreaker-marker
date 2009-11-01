c86efd220668afd3f2a50e9516ab9161173184a6
Corey Haines <coreyhaines@gmail.com>
2009-11-01 11:16:17 -0500

    update names

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'codebreaker_marker'
+require 'marker'
 require 'spec'
 require 'spec/autorun'
 
