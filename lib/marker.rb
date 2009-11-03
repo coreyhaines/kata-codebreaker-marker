0bc772f7ff54f3487b41a2bd81507a853afb8919
Corey Haines <coreyhaines@gmail.com>
2009-11-03 18:22:11 -0500

    using a matcher

--- lib/marker.rb
+++ lib/marker.rb
@@ -1,8 +1,20 @@
 class Marker
   def initialize(secret)
+    @secret = secret
   end
   def guess(these)
+    @guess = these
   end
   def mark
+    ['p'] * count_of_positional_matches + 
+    ['m'] * count_of_non_positional_matches
+  end
+  def count_of_positional_matches
+    [@secret, @guess].transpose.inject(0) do |count, colors|
+      count + (colors[0] == colors[1] ? 1 : 0)
+    end
+  end
+  def count_of_non_positional_matches
+    (@secret & @guess).size - count_of_positional_matches
   end
 end
--- spec/marker_spec.rb
+++ spec/marker_spec.rb
@@ -1,2 +1,71 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+Spec::Matchers.define :mark do |guess|
+  match do |marker|
+    marker.guess (@guess = guess)
+    @actual_mark = marker.mark
+    @actual_mark == @mark
+  end
+  def with(mark)
+    @mark = mark
+    self
+  end
+  failure_message_for_should do |marker|
+    "#{@guess.inspect} expected to mark as:\n#{@mark.inspect}, but got\n#{@actual_mark.inspect}"
+  end
+end
+describe Marker do
+  let(:marker) { Marker.new %w{r g y b} }
+  def check_mark(guess, mark)
+    marker.should mark(guess).with(mark)
+  end
+  context "Only positional matches" do
+    context "1 match" do
+      it "marks as ['p']" do
+        check_mark(%w{r c c c}, ['p'])
+      end
+    end
+    context "2 matches" do
+      it "marks as ['p']" do
+        check_mark(%w{r c c b}, ['p'] * 2)
+      end
+    end
+    context "3 matches" do
+      it "marks as ['p', 'p', 'p']" do
+        check_mark(%w{r g c b}, ['p'] * 3)
+      end
+    end
+    context "4 matches" do
+      it "marks as ['p', 'p', 'p', 'p']" do
+        check_mark(%w{r g y b}, ['p'] * 4)
+      end
+    end
+  end
+  context "Only non-positional matches" do
+    context "0 matches" do
+      it "marks as []" do
+        check_mark(%w{c c c c}, [])
+      end
+    end
+    context "1 matches" do
+      it "marks as ['m']" do
+        check_mark(%w{b c c c}, ['m'])
+      end
+    end
+    context "2 matches" do
+      it "return ['m', 'm']" do
+        check_mark(%w{b y c c}, ['m'] * 2)
+      end
+    end
+    context "3 matches" do
+      it "marks as ['m', 'm', 'm']" do
+        check_mark(%w{b y g c}, ['m'] * 3)
+      end
+    end
+    context "4 matches" do
+      it "marks as ['m', 'm', 'm', 'm']" do
+        check_mark(%w{b y g r}, ['m'] * 4)
+      end
+    end
+  end
+end
